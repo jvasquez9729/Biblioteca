5c807fe8eac87dca41410c9df13b6d4d2e25d7ba
jvasquez9729 <jhonjairocamilovasquez@gmail.com>
2025-05-11 21:40:41 -0500

    Add files via upload

---
+++ biblioteca.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-05-2025 a las 02:46:01
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autores`
+--
+
+CREATE TABLE `autores` (
+  `ID_Autor` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Nacionalidad` varchar(50) NOT NULL,
+  `Fecha_nacimiento` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `autores`
+--
+
+INSERT INTO `autores` (`ID_Autor`, `Nombre`, `Nacionalidad`, `Fecha_nacimiento`) VALUES
+(1, 'Gabriel García Márquez', 'Colombiana', '1927-03-06'),
+(2, 'J.K. Rowling', 'Británica', '1965-07-31'),
+(3, 'George Orwell', 'Británica', '1903-06-25');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `ID_Categoria` int(11) NOT NULL,
+  `Nombre_Categoria` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`ID_Categoria`, `Nombre_Categoria`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `ID_Libro` int(11) NOT NULL,
+  `Titulo` varchar(250) NOT NULL,
+  `ID_Autor` int(11) DEFAULT NULL,
+  `ID_Categoria` int(11) DEFAULT NULL,
+  `Año_publicacion` int(11) DEFAULT NULL,
+  `Disponible` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`ID_Libro`, `Titulo`, `ID_Autor`, `ID_Categoria`, `Año_publicacion`, `Disponible`) VALUES
+(1, 'Cien Años de Soledad', 1, 1, 1967, 0),
+(2, 'Harry Potter y la Piedra Filosofal', 2, 3, 1997, 1),
+(3, '1984', 3, 2, 1949, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos`
+--
+
+CREATE TABLE `prestamos` (
+  `ID_Prestamos` int(11) NOT NULL,
+  `ID_usuarios` int(11) DEFAULT NULL,
+  `ID_Libro` int(11) DEFAULT NULL,
+  `Fecha_Prestamo` date DEFAULT NULL,
+  `Fecha_Devolucion` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `prestamos`
+--
+
+INSERT INTO `prestamos` (`ID_Prestamos`, `ID_usuarios`, `ID_Libro`, `Fecha_Prestamo`, `Fecha_Devolucion`) VALUES
+(1, 1, 1, '2024-08-01', '2024-08-15'),
+(2, 2, 2, '2024-08-02', '2024-08-16');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID_usuarios` int(11) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Direccion` varchar(200) DEFAULT NULL,
+  `Telefono` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`ID_usuarios`, `Nombre`, `Direccion`, `Telefono`) VALUES
+(1, 'Carlos Martínez', 'Calle Luna, 123', '123456789'),
+(2, 'Lucía Fernández', 'Avenida Sol, 456', '987654321');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `autores`
+--
+ALTER TABLE `autores`
+  ADD PRIMARY KEY (`ID_Autor`);
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`ID_Categoria`);
+
+--
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`ID_Libro`),
+  ADD KEY `ID_Autor` (`ID_Autor`),
+  ADD KEY `ID_Categoria` (`ID_Categoria`);
+
+--
+-- Indices de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD PRIMARY KEY (`ID_Prestamos`),
+  ADD KEY `ID_usuarios` (`ID_usuarios`),
+  ADD KEY `ID_Libro` (`ID_Libro`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID_usuarios`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `autores`
+--
+ALTER TABLE `autores`
+  MODIFY `ID_Autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `ID_Categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `ID_Libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  MODIFY `ID_Prestamos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD CONSTRAINT `libros_ibfk_1` FOREIGN KEY (`ID_Autor`) REFERENCES `autores` (`ID_Autor`),
+  ADD CONSTRAINT `libros_ibfk_2` FOREIGN KEY (`ID_Categoria`) REFERENCES `categorias` (`ID_Categoria`);
+
+--
+-- Filtros para la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`ID_usuarios`) REFERENCES `usuarios` (`ID_usuarios`),
+  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`ID_Libro`) REFERENCES `libros` (`ID_Libro`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
